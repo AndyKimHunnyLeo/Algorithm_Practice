535d48dfb8a0c8d552f2f7a43a2f7a7b1a504569
Andy Kim <hyungSeok.kim@outlook.com>
2021-04-25 09:40:32 +1200

    BestAlbum algorithm instruction added

--- src/BestAlbum/Main.java
+++ src/BestAlbum/Main.java
@@ -3,6 +3,23 @@ package BestAlbum;
 import java.util.*;
 
 public class Main {
+    /*
+        We are trying to release the best album by collecting the two most played songs by genre on the streaming site.
+        Songs are identified by a unique number, and the criteria for recording songs are as follows.
+
+        1. The genre in which many songs are played are listed first.
+        2. Songs that have been played a lot within the genre are recorded first.
+        3. Among songs with the same number of plays within a genre, songs with a lower unique number are recorded first.
+
+        Given a string array genres representing the genre of a song and an integer array plays representing the number of times played per song,
+        complete the solution function to return the unique numbers of the songs to be in the best album in order.
+
+        * genres[i] is the genre of the song with the unique number i
+        * plays[i] is the number of times a song with unique number i has been played
+        * The lengths of genres and plays are the same, which is 1 or more and 10,000 or less. There are less than 100 genre types.
+        * If there is only one song in the genre, select only one song
+        * Every genre has a different number of plays
+     */
     public static void main(String[] args) {
         //dummy Data
         String[] genres = {"classic", "pop", "classic", "classic", "pop"};
